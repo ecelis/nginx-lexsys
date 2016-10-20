cb996a94fb638834d930475084f7c9587852fc38
Ernesto Celis <developer@celisdelafuente.net>
2016-10-20 03:41:40 -0500

    ADD nginx.spec.patch

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
-FROM centos:latest
+FROM centos:6
 MAINTAINER Ernesto Celis <ernesto@tic.uno>
 
 COPY src /
+# rpmbuild command recommends to use `builder:builder` as user:group.
+RUN yum -y install epel-release ; \
+  yum -y update ; \
+  yum install -y openssl-devel zlib-devel pcre-devel rpm-build make gcc \
+     perl-devel perl-ExtUtils-Embed GeoIP GeoIP-devel libxslt libxslt-devel \
+     gd gd-devel which ; \
+  useradd -u 1000 builder ; \
+  chmod a+x /build.sh
 
-ENTRYPOINT ["/bin/bash"]
-
-CMD ["./bootstrap.sh ; ./build.sh"]
+WORKDIR /home/builder
--- src/build.sh
+++ src/build.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+
 OS_VERSION=$(cat /etc/redhat-release)
-cd /root/rpmbuild/SPECS
+NHMTAG=v0.31
+NHMURL=https://github.com/openresty/headers-more-nginx-module/archive/${NHMTAG}.tar.gz
+if [[ $OS_VERSION == *" 7."* ]]; then
+  NGINXRPM=nginx-1.10.1-1.el7.ngx.src.rpm
+  RPMURL=http://nginx.org/packages/centos/7/SRPMS/
+elif [[ $OS_VERSION == *" 6."* ]]; then
+  NGINXRPM=nginx-1.10.1-1.el6.ngx.src.rpm
+  RPMURL=http://nginx.org/packages/centos/6/SRPMS/
+fi
+curl -LO $RPMURL/$NGINXRPM
+rpm -Uvh $NGINXRPM
+cd $HOME/rpmbuild/SOURCES
+curl -L -o ${NHMTAG}.tar.gz ${NHMURL}
+
+cd $HOME/rpmbuild/SPECS
 cp nginx.spec nginx.spec.orig
-patch -p0 < /vagrant/nginx.spec.patch
+patch -p0 < /nginx.spec.patch
 rpmbuild -ba nginx.spec
-cp /root/rpmbuild/SRPMS/*.rpm /mnt/out
-cp /root/rpmbuild/RPMS/x86_64/*.rpm /mnt/out
+#cp /root/rpmbuild/SRPMS/*.rpm /mnt/out
+#cp /root/rpmbuild/RPMS/x86_64/*.rpm /mnt/out
